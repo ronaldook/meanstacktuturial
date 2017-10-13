2d91d02aa6b79a72d63969a23431c04da8cab39a
David Chen <sun@computer-9.home>
2017-10-14 05:37:38 +0800

    Authentication Backend2

--- config/database.js
+++ config/database.js
@@ -1,7 +1,8 @@
 const crypto = require('crypto').randomBytes(256).toString('hex');
+// cryptographic functionality ()
 
 module.exports = {
-  uri: 'mongodb://localhost:27017/' + this.db,
-  secret: crypto,
-  db: 'mean-angular-2'
+  uri: 'mongodb://localhost:27017/mean-angular-2',  //Database URI and database name
+  secret: crypto,   //Cryto-created secret
+  db: 'mean-angular-2'  // Database name Do you know commit This is for you
 }
--- routes/authentication.js
+++ routes/authentication.js
@@ -6,10 +6,30 @@ module.exports = (router) => {
     //req.body.email
     //req.body.username
     //req.body.password
-    if(!req.body.email){
-      res.json({succes: false, message: 'You must provide an email'});
+    if(!req.body.email) {
+      res.json({ succes: false, message: 'You must provide an email'});
     } else {
-      res.send('Hello World');
+      if(!req.body.username) {
+        res.json({ succes: false, message: 'You must provide an name'});
+      } else {
+        if(!req.body.password) {
+          res.json({ succes: false, message: 'You must provide an password'});
+        } else {
+          let user = new User({
+            email: req.body.email.toLowerCase(),
+            username: req.body.username.toLowerCase(),
+            password: req.body.password.toLowerCase()
+          });
+          user.save((err) => {
+            if (err){
+              res.json({ success: false, message: 'Could not save user. Error : ', err});
+            }
+            else{
+              res.json({ success: true, message: 'User saved!'});
+            }
+          });
+        }
+      }
     }
   });
   return router;
